d3679d29e02b117233291aaf52819a56e1b7eb09
Derek <dqi@toronto.ca>
2025-12-11 16:26:03 -0500

    fix_json_serializability should now be working

--- src/nicegui_aggrid/fix_json_serializability.py
+++ src/nicegui_aggrid/fix_json_serializability.py
@@ -3,11 +3,22 @@ from typing import Any
 from epicstuff import Dict
 from collections.abc import Mapping, Iterable, Callable
 from nicegui.json import orjson_wrapper as json
+import orjson
+from nicegui.observables import ObservableDict, ObservableList
 
+
+# make nicegui run to_json on subclasses
+json.ORJSON_OPTS |= orjson.OPT_PASSTHROUGH_SUBCLASS
 
 # to json
 original_converter = json._orjson_converter
 def to_json(obj: Any) -> Any:
+	# treat nicegui dict and list as normal dict and list
+	if isinstance(obj, ObservableDict):
+		return dict(obj)
+	if isinstance(obj, ObservableList):
+		return dict(obj)
+
 	if hasattr(obj, '_to_json'):
 		data = obj._to_json()
 	elif isinstance(obj, Mapping):
@@ -15,7 +26,7 @@ def to_json(obj: Any) -> Any:
 	elif isinstance(obj, Iterable):
 		data = list(obj)
 	else:
-		data = original_converter(obj)
+		return original_converter(obj)
 	return {'__type__': obj.__class__.__name__, '__data__': data}
 
 
@@ -42,6 +53,9 @@ def _convert(item: Any) -> Any:
 			decoder = type_registry.get(type_name)
 			# if found, use it to decode the data
 			if decoder is not None:
+				# if decoder has from_json, use it
+				if hasattr(decoder, 'from_json') and callable(decoder.from_json):
+					return decoder.from_json(data)
 				return decoder(data)
 			return data
 		# else, convert each value in the dict
--- test/fix_json_serializability.py
+++ test/fix_json_serializability.py
@@ -4,10 +4,15 @@ from nicegui_aggrid.fix_json_serializability import load, type_registry
 
 from collections import deque
 
+
+class my_dict_subclass(dict): ...
+
+
 type_registry.Dict = Dict
 type_registry.deque = deque
 storage = load(app.storage.general)
 
-storage.setdefault('test1', deque([deque([deque([1, 2, 3]), 4, 5]), 6]))
+storage.setdefault('test1', deque([deque(), deque([Dict(a=10, b=11), 2, 3]), 4, my_dict_subclass(c=5)]))
 
-print(storage)
+# make sure to run twice
+assert storage.test1 == deque([deque(), deque([Dict(a=10, b=11), 2, 3]), 4, my_dict_subclass(c=5)])
